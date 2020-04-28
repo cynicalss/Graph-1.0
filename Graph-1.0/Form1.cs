bd44c70bcc26c6081d137ad964474fe54f32891f
SELJER <SELJER@SELJER>
2020-04-29 01:32:12 +0300

    branch test

--- Graph-1.0/Form1.cs
+++ Graph-1.0/Form1.cs
@@ -12,7 +12,8 @@ namespace Graph_1._1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+   //branch test  
+   public Form1()
         {
             InitializeComponent();
         }
