4b7de41fbb5bb41e1311ec1c5e540bbb08e8b46e
SELJER <SELJER@SELJER>
2020-04-29 01:06:10 +0300

    v1

---
+++ Graph-1.0/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Graph_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddNode = new System.Windows.Forms.Button();
+            this.btnRemoveNode = new System.Windows.Forms.Button();
+            this.btnAddArc = new System.Windows.Forms.Button();
+            this.btnRemoveArc = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnDFS = new System.Windows.Forms.Button();
+            this.btnBFS = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnAddNode
+            // 
+            this.btnAddNode.Location = new System.Drawing.Point(101, 122);
+            this.btnAddNode.Name = "btnAddNode";
+            this.btnAddNode.Size = new System.Drawing.Size(104, 43);
+            this.btnAddNode.TabIndex = 2;
+            this.btnAddNode.Text = "Add";
+            this.btnAddNode.UseVisualStyleBackColor = true;
+            this.btnAddNode.Click += new System.EventHandler(this.btnAddNode_Click);
+            // 
+            // btnRemoveNode
+            // 
+            this.btnRemoveNode.Location = new System.Drawing.Point(438, 122);
+            this.btnRemoveNode.Name = "btnRemoveNode";
+            this.btnRemoveNode.Size = new System.Drawing.Size(104, 43);
+            this.btnRemoveNode.TabIndex = 3;
+            this.btnRemoveNode.Text = "Remove";
+            this.btnRemoveNode.UseVisualStyleBackColor = true;
+            this.btnRemoveNode.Click += new System.EventHandler(this.btnRemoveNode_Click);
+            // 
+            // btnAddArc
+            // 
+            this.btnAddArc.Location = new System.Drawing.Point(149, 547);
+            this.btnAddArc.Name = "btnAddArc";
+            this.btnAddArc.Size = new System.Drawing.Size(116, 43);
+            this.btnAddArc.TabIndex = 4;
+            this.btnAddArc.Text = "Add";
+            this.btnAddArc.UseVisualStyleBackColor = true;
+            this.btnAddArc.Click += new System.EventHandler(this.btnAddArc_Click);
+            // 
+            // btnRemoveArc
+            // 
+            this.btnRemoveArc.Location = new System.Drawing.Point(149, 612);
+            this.btnRemoveArc.Name = "btnRemoveArc";
+            this.btnRemoveArc.Size = new System.Drawing.Size(116, 43);
+            this.btnRemoveArc.TabIndex = 5;
+            this.btnRemoveArc.Text = "Remove";
+            this.btnRemoveArc.UseVisualStyleBackColor = true;
+            this.btnRemoveArc.Click += new System.EventHandler(this.btnRemoveArc_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(1052, 29);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(104, 43);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "Graph";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnDFS
+            // 
+            this.btnDFS.Location = new System.Drawing.Point(1051, 221);
+            this.btnDFS.Name = "btnDFS";
+            this.btnDFS.Size = new System.Drawing.Size(105, 43);
+            this.btnDFS.TabIndex = 7;
+            this.btnDFS.Text = "DFS";
+            this.btnDFS.UseVisualStyleBackColor = true;
+            this.btnDFS.Click += new System.EventHandler(this.btnDFS_Click);
+            // 
+            // btnBFS
+            // 
+            this.btnBFS.Location = new System.Drawing.Point(1052, 312);
+            this.btnBFS.Name = "btnBFS";
+            this.btnBFS.Size = new System.Drawing.Size(105, 43);
+            this.btnBFS.TabIndex = 8;
+            this.btnBFS.Text = "BFS";
+            this.btnBFS.UseVisualStyleBackColor = true;
+            this.btnBFS.Click += new System.EventHandler(this.btnBFS_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1051, 577);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 43);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(48, 335);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 184);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(241, 335);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(132, 184);
+            this.listBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Node Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Node Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Arc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "From Node";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(237, 294);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "To Node";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(405, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 28);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(467, 221);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(540, 357);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 667);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBFS);
+            this.Controls.Add(this.btnDFS);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnRemoveArc);
+            this.Controls.Add(this.btnAddArc);
+            this.Controls.Add(this.btnRemoveNode);
+            this.Controls.Add(this.btnAddNode);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddNode;
+        private System.Windows.Forms.Button btnRemoveNode;
+        private System.Windows.Forms.Button btnAddArc;
+        private System.Windows.Forms.Button btnRemoveArc;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnDFS;
+        private System.Windows.Forms.Button btnBFS;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Graph-1.0/Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Graph_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Vertex
+        {
+            public bool wasVisited;
+            public string label;
+
+            public Vertex(string label)
+            {
+                this.label = label;
+                wasVisited = false;
+            }
+        }
+        public class Graph
+        {
+            private const int NUM_VERTICES = 15;
+            private Vertex[] vertices;
+            private int[,] adjMatrix;
+            private int numVerts;
+
+            public Graph()
+            {
+                vertices = new Vertex[NUM_VERTICES];
+                adjMatrix = new int[NUM_VERTICES, NUM_VERTICES];
+                numVerts = 0;
+                for (int j = 0; j < NUM_VERTICES; j++)
+                {
+                    for (int k = 0; k < NUM_VERTICES; k++)
+                    {
+                        adjMatrix[j, k] = 0;
+                    }
+                }
+            }
+
+            public bool AddVertex(string label)
+            {
+                try
+                {
+                    vertices[numVerts] = new Vertex(label);
+                    numVerts++;
+                    return true;
+                }
+                catch
+                {
+                    MessageBox.Show("ERR01");
+                    return false;
+                }
+            }
+
+            public void AddEdge(int start, int eend)
+            {
+                try
+                {
+                    adjMatrix[start, eend] = 1;
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+
+            public bool HasEdge(int start, int end)
+            {
+                if (adjMatrix[start, end] == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            public string ShowVertex(int v)
+            {
+                return (vertices[v].label);
+            }
+
+            public string ShowMatrix()
+            {
+                if (numVerts == 0)
+                {
+                    return "";
+                }
+
+                string s = "   |";
+                for (int i = 0; i < numVerts; i++)
+                {
+                    s += string.Format(" {0} |", ShowVertex(i));
+                }
+                s += "\n";
+
+                for (int i = 0; i <= numVerts; i++)
+                {
+                    s += "===|";
+                }
+                s += "\n";
+
+                for (int j = 0; j < numVerts; j++)
+                {
+                    s += string.Format(" {0} |", ShowVertex(j));
+                    for (int k = 0; k < numVerts; k++)
+                    {
+                        if (HasEdge(j, k))
+                        {
+                            s += " 1 |";
+                        }
+                        else
+                        {
+                            s += " 0 |";
+                        }
+                    }
+                    s += "\n";
+                }
+                return s;
+            }
+
+      public void ClearGraph()
+            {
+                for (int i = 0; i < NUM_VERTICES; i++)
+                {
+                    vertices[i] = null;
+                }
+
+                numVerts = 0;
+                for (int j = 0; j < NUM_VERTICES; j++)
+                {
+                    for (int k = 0; k < NUM_VERTICES; k++)
+                    {
+                        adjMatrix[j, k] = 0;
+                    }
+                }
+            }
+
+            public void DelEdge(int start, int eend)
+            {
+                try
+                {
+                    adjMatrix[start, eend] = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+
+            private void MoveRow(int row, int length)
+            {
+                for (int col = 0; col < length; col++)
+                {
+                    adjMatrix[row, col] = adjMatrix[row + 1, col];
+                }
+            }
+
+            private void MoveCol(int col, int length)
+            {
+                for (int row = 0; row < length; row++)
+                {
+                    adjMatrix[row, col] = adjMatrix[row, col + 1];
+                }
+            }
+
+            public bool DelVertex(int vert)
+            {
+                try
+                {
+                    if (numVerts == 0)
+                    {
+                        return true;
+                    }
+
+                    if (vert != numVerts - 1)
+                    {
+                        for (int j = vert; j < numVerts - 1; j++)
+                        {
+                            vertices[j] = vertices[j + 1];
+                        }
+
+                        for (int row = vert; row < numVerts - 1; row++)
+                        {
+                            MoveRow(row, numVerts);
+                        }
+
+                        for (int col = vert; col < numVerts - 1; col++)
+                        {
+                            MoveCol(col, numVerts);
+                        }
+                    }
+                    numVerts--;
+                    return true;
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                    return false;
+                }
+            }
+
+            public int Count()
+            {
+                return numVerts;
+            }
+
+            //Търсене в дълбочина
+            private int GetUnvisitedVertex(int v)
+            {
+                for (int j = 0; j < numVerts; j++)
+                {
+                    if ((adjMatrix[v, j] == 1) && (vertices[j].wasVisited == false))
+                    {
+                        return j;
+                    }
+                }
+                return -1;
+            }
+
+            public string DFS()
+            {
+                vertices[0].wasVisited = true;
+                string s = ShowVertex(0) + " -> ";
+                Stack gStack = new Stack();
+                gStack.Push(0);
+                int v;
+                while (gStack.Count > 0)
+                {
+                    v = GetUnvisitedVertex((int)gStack.Peek());
+                    if (v == -1)
+                    {
+                        gStack.Pop();
+                    }
+                    else
+                    {
+                        vertices[v].wasVisited = true;
+                        s += ShowVertex(v) + " -> ";
+                        gStack.Push(v);
+                    }
+                }
+                for (int j = 0; j < numVerts; j++)
+                {
+                    vertices[j].wasVisited = false;
+                }
+
+                return s;
+            }
+
+            //Търсене в ширина
+            public string BFS()
+            {
+                Queue gQueue = new Queue();
+                vertices[0].wasVisited = true;
+                string s = ShowVertex(0) + " -> ";
+                gQueue.Enqueue(0);
+                int vParent, vChild;
+                while (gQueue.Count > 0)
+                {
+                    vParent = (int)gQueue.Dequeue();
+                    vChild = GetUnvisitedVertex(vParent);
+                    while (vChild != -1)
+                    {
+                        vertices[vChild].wasVisited = true;
+                        s += ShowVertex(vChild) + " -> ";
+                        gQueue.Enqueue(vChild);
+                        vChild = GetUnvisitedVertex(vParent);
+                    }
+                }
+                for (int i = 0; i < numVerts; i++)
+                {
+                    vertices[i].wasVisited = false;
+                }
+
+                return s;
+            }
+
+           
+
+            public int NoSuccessors()
+            {
+                bool isEdge;
+                for (int row = 0; row < numVerts; row++)
+                {
+                    isEdge = false;
+                    for (int col = 0; col < numVerts; col++)
+                    {
+                        if (adjMatrix[row, col] > 0)
+                        {
+                            isEdge = true;
+                            break;
+                        }
+                    }
+                    if (!(isEdge))
+                    {
+                        return row;
+                    }
+                }
+                return -1;
+            }
+
+        }
+
+        Graph g = new Graph();
+
+        private void CreateGraph(string[] items)
+        {
+            g.ClearGraph();
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(items);
+            listBox1.SelectedIndex = 0;
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(items);
+            listBox2.SelectedIndex = 0;
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(items);
+            comboBox1.SelectedIndex = 0;
+            foreach (string element in items)
+            {
+                g.AddVertex(element);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string[] items = new string[] { "A", "B", "C", "D", "E", "F" };
+            CreateGraph(items);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 3);
+            g.AddEdge(3, 4);
+            g.AddEdge(4, 5);
+            g.AddEdge(1, 5);
+
+            richTextBox1.Text = g.ShowMatrix();
+        }
+
+        private void btnDFS_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DFS: \n" + g.DFS());
+        }
+
+        private void btnBFS_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("BFS: \n" + g.BFS());
+        }
+
+        private void btnAddNode_Click(object sender, EventArgs e)
+        {
+            string labelVertex = textBox1.Text;
+            if (labelVertex.Length > 0)
+            {
+                if (g.AddVertex(labelVertex))
+                {
+                    listBox1.Items.Add(labelVertex);
+                    listBox2.Items.Add(labelVertex);
+                    comboBox1.Items.Add(labelVertex);
+
+                    textBox1.Text = "";
+                    richTextBox1.Text = g.ShowMatrix();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No label");
+            }
+        }
+
+        private void btnRemoveNode_Click(object sender, EventArgs e)
+        {
+            if (g.Count() == 0)
+            {
+                return;
+            }
+
+            int row = int.Parse(comboBox1.SelectedIndex.ToString());
+
+            if (row > -1 && g.DelVertex(row))
+            {
+                comboBox1.Items.Remove(comboBox1.SelectedIndex.ToString());
+                int count = comboBox1.Items.Count;
+                string[] items = new string[count];
+                for (int i = 0; i < count; i++)
+                {
+                    items[i] = comboBox1.Items[i].ToString();
+                }
+
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(items);
+
+                listBox2.Items.Clear();
+                listBox2.Items.AddRange(items);
+
+                comboBox1.Text = "";
+
+                richTextBox1.Text = g.ShowMatrix();
+                if (g.Count() > 0)
+                {
+                    listBox1.SelectedIndex = 0;
+                    listBox2.SelectedIndex = 0;
+                    comboBox1.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void btnAddArc_Click(object sender, EventArgs e)
+        {
+            int row = int.Parse(listBox1.SelectedIndex.ToString());
+            int col = int.Parse(listBox2.SelectedIndex.ToString());
+            g.AddEdge(row, col);
+            richTextBox1.Text = g.ShowMatrix();
+        }
+
+        private void btnRemoveArc_Click(object sender, EventArgs e)
+        {
+            int row = int.Parse(listBox1.SelectedIndex.ToString());
+            int col = int.Parse(listBox2.SelectedIndex.ToString());
+            g.DelEdge(row, col);
+            richTextBox1.Text = g.ShowMatrix();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+    }
+}
